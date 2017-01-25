58e0f53c591ccfd7fb7dd747474b0ac77e03d3f3
Cphil31 <cecile_philippe@live.fr>
2017-01-25 11:41:15 +0100

    le dictionnaire contient  mots qui font exactement 15 caractères

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
-echo strlen($string);
 
- ?>
+$find=strlen($string);
+
+
+echo "le dictionnaire contient ".$find.' mots.';
+$val=0;
+foreach ($dico as $key => $value) {
+if(strlen($value)===15){
+	$val++;
+	}	
+}
+	echo "le dictionnaire contient ".strval($val).' mots qui font exactement 15 caractères .';
+
+
